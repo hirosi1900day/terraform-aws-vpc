35a313b10ed3f955b3f4b0ffd930d9e902173750
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-22 20:50:54 +0900

    variablesの変数の説明を追加

--- variables.tf
+++ variables.tf
@@ -1,18 +1,24 @@
 variable "system" {
   type = string
+  description = "システム名"
 }
 variable "env" {
   type = string
+  description = "環境名"
 }
 variable "cidr_vpc" {
   type = string
+  description = "vpcのCIDR"
 }
 variable "cidr_public" {
   type = list(string)
+  description = "public subnetのCIDR"
 }
 variable "cidr_private" {
   type = list(string)
+  description = "private subnetのCIDR"
 }
 variable "cidr_secure" {
   type = list(string)
+  description = "secure subnetのCIDR"
 }
