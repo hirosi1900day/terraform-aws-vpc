8b2f2398bfe780e457d79865d1a62ee796a72f1f
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-03-22 21:04:36 +0900

    リファクタ

--- variables.tf
+++ variables.tf
@@ -2,22 +2,27 @@ variable "system" {
   type = string
   description = "システム名"
 }
+
 variable "env" {
   type = string
   description = "環境名"
 }
+
 variable "cidr_vpc" {
   type = string
   description = "vpcのCIDR"
 }
+
 variable "cidr_public" {
   type = list(string)
   description = "public subnetのCIDR"
 }
+
 variable "cidr_private" {
   type = list(string)
   description = "private subnetのCIDR"
 }
+
 variable "cidr_secure" {
   type = list(string)
   description = "secure subnetのCIDR"
